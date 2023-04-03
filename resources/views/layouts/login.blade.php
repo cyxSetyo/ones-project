4a9c2d2a8dc664f4d4984d549240812f13971744
cyxSetyo <cahyasetya003@gmail.com>
2023-04-03 16:43:41 +0700

    test 03 04

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -1,25 +1,33 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="/css/style.css">
-</head>
-    <body>
-        <div class="container">
-            <form action="" method="POST">
-                @csrf
-                <div class="usr-lgn">
-                    <label for="username">Username</label>
-                    <input type="text" placeholder="Username">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap demo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/style.css">
+    </head>
+        <body>
+            <div>
+                <div>
+                    <form action="" method="post">
+
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Username</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Username">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
                 </div>
-                <div class="usr-pass">
-                    <label for="passoword">Password</label>
-                    <input type="text" placeholder="Password">
-                </div>
-            </form>
-        </div>
-    </body>
+            </div>
+
+
+
+  
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+        </body>
 </html>
