d22c5788e501659cd96da858116b1d2e118864c0
cyxSetyo <98861415+cyxSetyo@users.noreply.github.com>
2023-06-30 16:37:41 +0700

    left bar

--- resources/views/template/leftNavbar.blade.php
+++ resources/views/template/leftNavbar.blade.php
@@ -1,9 +1,14 @@
 <div class="navbar-left">
-    <div class="navbar-icon">
-        <img src="/img/icon.jpg" alt="icon">
-        <div class="desc">
-            <p>Minion Plans Store</p>
-            <p class="status">Status : Active</p>
+    <div class="content">
+        <div class="navbar-icon">
+            <img src="/img/icon.jpg" alt="icon">
+            <div class="desc">
+                <h4>Minion Plans Store</h4>
+                <p class="status">Status : Open</p>
+            </div>
+        </div>
+        <div class="describtion">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam nemo magnam corporis repudiandae porro obcaecati, libero aliquid culpa harum sequi consequatur inventore voluptas. Ad saepe laborum nemo explicabo, architecto molestiae?</p>
         </div>
     </div>
 </div>
